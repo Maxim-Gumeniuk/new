a2217d589908e8f0d6d50424e5ad45382b0ff483
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-04-18 22:21:15 +0300

    change burger width

--- src/styled/burger-menu/container.ts
+++ src/styled/burger-menu/container.ts
@@ -3,7 +3,7 @@ import styled, { DefaultTheme, ThemedStyledProps } from 'styled-components';
 import { MyGlobalStyle } from '../global';
 
 export const BurgerContainer = styled(motion.div)`
-  width: 30%;
+  width: 100%;
   border-right: 2px solid
     ${(props: ThemedStyledProps<MyGlobalStyle, DefaultTheme>) =>
       props.theme.colors.main};
